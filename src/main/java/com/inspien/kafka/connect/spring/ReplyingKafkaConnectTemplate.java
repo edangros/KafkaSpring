95ca8d30a0a50db98d60940ab86c45b4d0e6f6f6
edangros <edangros@gmail.com>
2021-07-14 11:22:20 +0900

    remove spring contexts

--- src/main/java/com/inspien/kafka/connect/spring/ReplyingKafkaConnectTemplate.java
+++ src/main/java/com/inspien/kafka/connect/spring/ReplyingKafkaConnectTemplate.java
@@ -25,7 +25,6 @@ import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.util.ConnectUtils;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -33,7 +32,6 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.kafka.KafkaException;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.listener.BatchMessageListener;
 import org.springframework.kafka.listener.GenericMessageListenerContainer;
 import org.springframework.kafka.requestreply.CorrelationKey;
@@ -43,16 +41,19 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.util.Assert;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.concurrent.SettableListenableFuture;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
+import org.apache.kafka.connect.source.SourceTask;
+import org.apache.kafka.clients.producer.Producer;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Almost Same Spring-Kafka's {@link Kafkatemplate} and {@link ReplyingKafkaTemplate} but uses {@link SourceTask}s instead of {@link Producer}s.
- * 
+ * The counterpart of Spring-Kafka's {@link KafkaTemplate} and {@link ReplyingKafkaTemplate} which uses {@link SourceTask}s instead of {@link Producer}s.
+ * But runs out of Spring's boundary, to 
  */
 @Slf4j
-public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[], byte[]>, InitializingBean, SmartLifecycle,
-        ApplicationContextAware, ApplicationListener<ContextStoppedEvent>, DisposableBean{
+public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[], byte[]> {
     
     private static final long DEFAULT_REPLY_TIMEOUT = 5000L;
 
@@ -81,9 +82,9 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         Assert.notNull(container, "'replyContainer' cannot be null");
         this.replyContainer = container;
         this.replyContainer.setupMessageListener(this);
-        this.converter = new JsonConverter();
         this.connectionID = connectionId;
         this.replyTimeout = replyTimeout;
+        this.converter = new JsonConverter();
     }
 
     public ReplyingKafkaConnectTemplate(GenericMessageListenerContainer<byte[], byte[]> container, String connectionId) {
@@ -101,29 +102,6 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         this.replyTimeout = replyTimeout;
     }
 
-    @Override
-    public boolean isRunning() {
-        return this.running;
-    }
-
-    @Override
-    public int getPhase() {
-        return this.phase;
-    }
-
-    public void setPhase(int phase) {
-        this.phase = phase;
-    }
-
-    @Override
-    public boolean isAutoStartup() {
-        return this.autoStartup;
-    }
-
-    public void setAutoStartup(boolean autoStartup) {
-        this.autoStartup = autoStartup;
-    }
-
     /**
      * Return the topics/partitions assigned to the replying listener container.
      * 
@@ -133,14 +111,12 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         return this.replyContainer.getAssignedPartitions();
     }
 
-    @Override
     public void afterPropertiesSet() throws Exception {
         if (!this.schedulerSet) {
             ((ThreadPoolTaskScheduler) this.scheduler).initialize();
         }
     }
 
-    @Override
     public synchronized void start() {
         if (!this.running) {
             try {
@@ -153,7 +129,6 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         }
     }
 
-    @Override
     public synchronized void stop() {
         if (this.running) {
             this.running = false;
@@ -162,14 +137,13 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         }
     }
 
-    @Override
     public void stop(Runnable callback) {
         stop();
         callback.run();
     }
 
     public SettableListenableFuture<SinkRecord> sendAndReceive(SourceRecord record, Duration replyTimeout) {
-        Assert.state(this.running, "Template has not been start()ed"); // NOSONAR (sync)
+        Assert.state(this.running, "Template has not been started"); // NOSONAR (sync)
         CorrelationKey correlationId = createCorrelationId(record);
         Assert.notNull(correlationId, "the created 'correlationId' cannot be null");
         record.headers().add(KafkaHeaders.CORRELATION_ID,
@@ -200,13 +174,6 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
             }
         }, Instant.now().plusMillis(replyTimeout.toMillis()));
         return future;
-    }
-
-    @Override
-    public void destroy() {
-        if (!this.schedulerSet) {
-            ((ThreadPoolTaskScheduler) this.scheduler).destroy();
-        }
     }
 
     /**
@@ -268,7 +235,7 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         try{
             keyAndSchema = converter.toConnectData(record.topic(), record.headers(), record.key());
             valueAndSchema = converter.toConnectData(record.topic(), record.headers(), record.value());
-            //read kafka header and conver to connect header
+            //read kafka header and convert to connect header
             headers = new ConnectHeaders();
             org.apache.kafka.common.header.Headers recordHeaders = record.headers();
             if (recordHeaders != null) {
@@ -297,18 +264,5 @@ public class ReplyingKafkaConnectTemplate implements BatchMessageListener<byte[]
         // Error reporting will need to correlate each sink record with the original consumer record
         return connectRecord;
     }
-
-
-    @Override
-    public void onApplicationEvent(ContextStoppedEvent event) {
-		//Do nothing - We have No producer while Kafkatemplate has.
-        //Tasks will stop by themselves.
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-		this.applicationContext = applicationContext;
-    }
-
 
 }
